a6d49b569c041ee4a2f98195df3587c956ba25ab
Krishna Pandey <129619885+Krixhnnna@users.noreply.github.com>
2024-12-27 21:54:27 +0530

    Create 70. Climbing Stairs.java

---
+++ 70. Climbing Stairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 3) return n;
+        int a = 0, b = 1;
+        for (int i = 1; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+}}
