7f4a4be4638eac9291096626729076f978c51118
Krishna Pandey <129619885+Krixhnnna@users.noreply.github.com>
2025-01-06 22:14:19 +0530

    Create 35. Search Insert Position.java

---
+++ 35. Search Insert Position.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = nums.length;
+        for(int i= 0; i<l;i++){
+            if(nums[i]==target || nums[i]>target){
+                return i;
+            }
+        }
+        return l;
+    }
+}
